75854bd4bffe291d279b62f6aeb6e64dd3fa0253
(Abby) Abhishek Luv <abhishek.luv@outlook.com>
2024-04-22 18:26:03 +0530

    Create math.service.ts

---
+++ math.service.ts
@@ -0,0 +1,7 @@
+export class MathService { // Define a simple service class
+  constructor() {}
+
+  add(num1: number, num2: number): number { // Method to add two numbers
+    return num1 + num2; // Return the sum
+  }
+}
